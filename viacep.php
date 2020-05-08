0416e5b62f601c09ea454a915d6235df77dc2a22
adrianooliveira20000000 <64998043+adrianooliveira20000000@users.noreply.github.com>
2020-05-07 23:54:53 -0300

    ok

---
+++ viacep.php
@@ -0,0 +1,22 @@
+<?php 
+
+$address = (object)[
+	'cep' => '',
+	'logradouro' => '',
+	'bairro' => '',
+	'localidade' => '',
+	'uf' => ''
+];
+ if (isset($_POST['cep'])) {
+ 	
+ $cep = $_POST['cep'];
+
+ $cep = preg_replace('/[^0-9]/','',$cep);
+ 
+
+ $url = "https://viacep.com.br/ws/{$cep}/json/";
+
+ $address = json_decode(file_get_contents($url));
+
+
+}
